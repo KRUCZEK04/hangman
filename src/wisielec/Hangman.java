9a2b88a9462583dfc0e873f8979d831f1345056f
Adam Kruk <krukadam15@gmail.com>
2023-11-16 17:02:03 +0100

    initial commit hangaman

---
+++ src/wisielec/Hangman.java
@@ -0,0 +1,10 @@
+package wisielec;
+//TODO: Hasło do zgadnięcia: obiekt który będzie wpisywany przy starcie programu albo tablice z hasłami (random) - to później.
+//TODO: Wisielec będzie się rysował w konsoli (od dołu do góry)
+//TODO: Musimy wpisywać i odgadywać litery (____) --> (___A_)
+//TODO: Program działa w pętli do momentu jeśli odgadniemy hasło || zostanie ukończony wisielec (skończą nam się życia)
+
+//TODO: Obiekty: Password, Hangman, PasswordGueser, LifeChecker
+public class Hangman {
+
+}
